85daca61cc53b75873ff987f56b7fd090910e0df
Reem Gawad <reemabdelg@gmail.com>
2023-03-09 16:57:04 -0500

    started fetch_restaurants method which calls Places API

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,19 @@
+require 'json'
+require 'open-uri'
+
 class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_restaurant, only: %i[show toggle_favorite]
 
   def index
-    # Four scenarios:
+    # Filter:
     # 1- No search values present
     # 2- Only location value
-    # 3- Only waittime value
-    # "wait_time < ?", "#{params[:waittime]}"
-    # 4- Both Location and Waittime
+    # 3- Only wait-time value
+    # 4- Both location and wait-time
     @restaurants = Restaurant.all.order(wait_time: :asc)
-    unless params[:location].blank?
-      @restaurants = @restaurants.where("address ILIKE ?", "%#{params[:location]}%")
-    end
-    unless params[:wait_time].blank?
-      @restaurants = @restaurants.where("wait_time <= ?", params[:wait_time])
-    end
+    @restaurants = @restaurants.where("address ILIKE ?", "%#{params[:location]}%") unless params[:location].blank?
+    @restaurants = @restaurants.where("wait_time <= ?", params[:wait_time]) unless params[:wait_time].blank?
 
     @markers = @restaurants.geocoded.map do |restaurant|
       {
@@ -35,20 +34,32 @@ class RestaurantsController < ApplicationController
         }
       end
     end
+    fetch_restaurants
+    raise
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @visit = Visit.new
     @visits = @restaurant.visits.where.not(arrived: false).order(date: :desc)
   end
 
   def toggle_favorite
-    @restaurant = Restaurant.find(params[:id])
     current_user.favorited?(@restaurant) ? current_user.unfavorite(@restaurant) : current_user.favorite(@restaurant)
   end
 
   def my_favorites
     @favorites = current_user.all_favorited
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def fetch_restaurants
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20Montreal&key=#{ENV['PLACES_API_KEY']}"
+    url_serialized = URI.open(url).read
+    @data = JSON.parse(url_serialized)["results"].first
   end
 end
